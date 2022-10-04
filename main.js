d53f24db697ab8d2a630fb5b2f30054f618ae134
Barret108 <barretappiah@gmail.com>
2022-10-04 13:53:42 -0600

    donezo

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 // JavaScript Code
+
+function myScore() {
+    var g1 = document.getElementById("dg1").value;
+    var g2 = document.getElementById("dg2").value;
+    var g3 = document.getElementById("dg3").value;
+    var g4 = document.getElementById("dg4").value;
+    var g5 = document.getElementById("dg5").value;
+    var score = 0;
+    var score = +g1 + +g2 + +g3 + +g4 + +g5;
+    grade = score / 5
+    document.getElementById("scorePercent").innerText = grade
+}
